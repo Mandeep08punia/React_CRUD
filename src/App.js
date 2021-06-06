2fb196dbd3d5b3fabf73f2f55c78a36bd1a247bb
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-06-06 13:33:42 +0530

    CRUD added #done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
-import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Navbar from './layout/Navbar'
-import AddUser from './users/AddUser'
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import NotFound from './components/NotFound';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./layout/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import NotFound from "./components/NotFound";
+import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import User from "./users/User";
 
-
-function App() {
+function App(props) {
   return (
     <Router>
       <div className="App">
-      <Navbar/>
-      <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/about" component={About}/>
-      <Route exact path="/contact" component={Contact}/>
-      <Route exact path="/users/add" component={AddUser}/>
-      <Route component={NotFound}/>
-      </Switch>
-    </div>
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/users/add" component={AddUser} />
+          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route exact path="/users/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from "react";
 
-function About() {
-    return (
-        <div className="container">
-            <div className="py-4"></div>
-            <h1>AboutPage</h1>
-            <p className="lead">ahjdg adgua hoghha  gad gd gula dgh had ughuds hgiuwehqwuehgudfhjkghdbcvbkadughau
-            ughudsagf adug hofduag dgdfh gdfh gsugyff g fdg dgufdughjrgrg
-             ghweruighrghe
-             g er giug erg irg rug eru gueri gu g yiurg ruiowryg qr gyq gyqqeruigyer g</p>
-             <p className="lead">ahjdg adgua hoghha  gad gd gula dgh had ughuds hgiuwehqwuehgudfhjkghdbcvbkadughau
-            ughudsagf adug hofduag dgdfh gdfh gsugyff g fdg dgufdughjrgrg
-             ghweruighrghe
-             g er giug erg irg rug eru gueri gu g yiurg ruiowryg qr gyq gyqqeruigyer g</p>
-             <p className="lead">ahjdg adgua hoghha  gad gd gula dgh had ughuds hgiuwehqwuehgudfhjkghdbcvbkadughau
-            ughudsagf adug hofduag dgdfh gdfh gsugyff g fdg dgufdughjrgrg
-             ghweruighrghe
-             g er giug erg irg rug eru gueri gu g yiurg ruiowryg qr gyq gyqqeruigyer g</p>
-        </div>
-    )
-}
+function About(){
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
+          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
+          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
+          laborum dolorum libero maiores!
+        </p>
 
-export default About
+        <p className="lead">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
+          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
+          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
+          laborum dolorum libero maiores!
+        </p>
+        <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt nam accusantium? Soluta cupiditate, accusamus commodi praesentium laborum dolorum libero maiores!</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
